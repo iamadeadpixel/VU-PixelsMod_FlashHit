0d721c1a9998fba8828d8770139a7bcfbd2fd5fe
Ton Muller <spatieman@gmail.com>
2025-01-31 15:55:09 +0100

    Update config2.lua

--- ext/Shared/config2.lua
+++ ext/Shared/config2.lua
@@ -1,6 +1,6 @@
 -- Here you can turn on, or off modules.
 Config2 = {
-	pixelmodspam = true, -- Turns on or off the console spam for pixelmod
+	pixelmodspam = false, -- Turns on or off the console spam for pixelmod
 	LevelPrint = false, -- Turns on or off the console spam for map names and gamemode
 	updatecheck = false -- On server boot only: Shows if there is a new version
 
